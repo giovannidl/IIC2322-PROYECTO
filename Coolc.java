9be545364f90fdb58e53ee5b6726b677b8e6a3af
Giovanni De Lucca <gio@gio-ubuntuD.(none)>
2013-04-05 23:58:25 -0300

    primera entrega

--- Coolc.java
+++ Coolc.java
@@ -68,7 +68,6 @@ public class Coolc {
 					    break;
 
 					case Parser.STRING:
-						System.err.println(lex.getLStrVal());
 						String texto = lex.getLStrVal().replaceAll("\n", "").replaceAll("\f", "");
 					    if(texto.length() <= 20)
 					    	System.out.printf("%s:%2d:%2d  String<%s>\n", f.getPath(), pos.line, pos.column, texto);
